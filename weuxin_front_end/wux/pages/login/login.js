83ac3cd7a8e40d5f08f6d651836181baf16e0c9d
hanzehao <530154757@qq.com>
2018-11-26 16:41:51 +0800

    上传前端微信小程序工程

---
+++ weuxin_front_end/wux/pages/login/login.js
@@ -0,0 +1,137 @@
+import { $wuxSelect } from '../../dist/index'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    head:"/images/1.png",
+    value1: '',
+    title1: '',
+    value2: '',
+    title2: '',
+    value3: '',
+    title3: '',
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  onClick1() {
+    $wuxSelect('#wux-select1').open({
+      value: this.data.value1,
+      options: [
+        'A小学',
+        'B小学',
+        'C小学',
+        'D小学',
+        'E小学',
+        'F小学',""
+      ],
+      onConfirm: (value, index, options) => {
+        console.log('onConfirm', value, index, options)
+        if (index !== -1) {
+          this.setData({
+            value1: value,
+            title1: options[index],
+          })
+        }
+      },
+    })
+  },
+  onChange(e) {
+    console.log('onChange', e)
+    this.setData({
+      error: isTel(e.detail.value),
+      value: e.detail.value,
+    })
+  },
+  onFocus(e) {
+    this.setData({
+      error: isTel(e.detail.value),
+    })
+    console.log('onFocus', e)
+  },
+  onBlur(e) {
+    this.setData({
+      error: isTel(e.detail.value),
+    })
+    console.log('onBlur', e)
+  },
+  onConfirm(e) {
+    console.log('onConfirm', e)
+  },
+  onClear(e) {
+    console.log('onClear', e)
+    this.setData({
+      error: true,
+      value: '',
+    })
+  },
+  onError() {
+    wx.showModal({
+      title: 'Please enter 11 digits',
+      showCancel: !1,
+    })
+  },
+  login:function(){
+    wx.switchTab({
+       url: '../main/main',
+    })
+  },
+})
+
