22fc1b33de18b7e6575c379a6b6cdb84e2264fb8
240436170 <240436170@qq.com>
2018-12-05 17:19:33 +0800

    .

--- weuxin_front_end/wux/pages/school_teacher/school_teacher.js
+++ weuxin_front_end/wux/pages/school_teacher/school_teacher.js
@@ -10,6 +10,13 @@ Page({
     title1: '8:00',
     value2: '',
     title2: '5:00',
+    s: '',
+    upschool1 :'7:00', 
+    upschool2: '8:00', 
+    upschool3: '8:30', 
+    downschool1:'4:00', 
+    downschool2: '5:00', 
+    downschool3: '5:30', 
   },
 
   /**
@@ -30,7 +37,89 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    var that = this
+    wx.request({
+      url: 'http://localhost:8080/weixin/schooltimecheck',
+      method: 'GET',
+      data: {
+        a:'1'
+      },
+      header: {
+        //  'content-type':'application/json'
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var a = res.data
+        that.setData({
+          s: a,
+        })
+        console.log(res.data)
+      }
+    })
+  },
 
+  upschoolinput: function (e) {
+    this.setData({
+      upschool1: e.detail.value
+    })
+    console.log(this.data.upschool1)
+  },
+  upschoolinput2: function (e) {
+    this.setData({
+      upschool2: e.detail.value
+    })
+    console.log(this.data.upschool2)
+  },
+  upschoolinput3: function (e) {
+    this.setData({
+      upschool3: e.detail.value
+    })
+    console.log(this.data.upschool3)
+  },
+  downschoolinput: function (e) {
+    this.setData({
+      downschool1: e.detail.value
+    })
+    console.log(this.data.downschool1)
+  },
+  downschoolinput2: function (e) {
+    this.setData({
+      downschool2: e.detail.value
+    })
+    console.log(this.data.downschool2)
+  },
+  downschoolinput3: function (e) {
+    this.setData({
+      downschool3: e.detail.value
+    })
+    console.log(this.data.downschool3)
+  },
+  // 提交上下学数据到后台
+  button:function(e){    
+    var that = this
+    wx.request({
+      url: 'http://localhost:8080/weixin/schooltime',
+      method: 'GET',
+      data: {
+        upschool1: this.data.upschool1,
+        upschool2: this.data.upschool2,
+        upschool3: this.data.upschool3,
+        downschool1:this.data.downschool1,
+        downschool2: this.data.downschool2,
+        downschool3: this.data.downschool3
+      },
+      header: {
+        //  'content-type':'application/json'
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var a = res.data
+        that.setData({
+          s: a,
+        })
+        console.log(res.data)
+      }
+    })
   },
 
   /**
