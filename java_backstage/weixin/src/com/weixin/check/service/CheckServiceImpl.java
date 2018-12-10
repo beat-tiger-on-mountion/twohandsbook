24fbbf65a6258b614e9f00c9ca302caef6e25391
xueyunqing <2404226102@qq.com>
2018-12-10 17:25:20 +0800

    完成了考勤的后台

--- java_backstage/weixin/src/com/weixin/check/service/CheckServiceImpl.java
+++ java_backstage/weixin/src/com/weixin/check/service/CheckServiceImpl.java
@@ -10,30 +10,41 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.weixin.check.dao.CheckDaoImpl;
 import com.weixin.entity.Check;
+import com.weixin.entity.Students;
+import com.weixin.entity.User;
+import com.weixin.users.dao.UserDaoImpl;
+
 /**
  * 
     * @ClassName: CheckServiceImpl  
-    * @Description: 查询请假和迟到学生  
+    * @Description: 考勤  
     * @author xueyunqing  
     * @date 2018年12月6日  
     *
  */
 @Service
-@Transactional(readOnly=true)
+@Transactional(readOnly = true)
 public class CheckServiceImpl {
 	@Resource
-    private CheckDaoImpl checkDaoImpl;
-	
-	//查询请假学生
-	public List<Check>  findAbsence(){
-		List<Check> list1=new ArrayList<Check>();
+	private CheckDaoImpl checkDaoImpl;
+
+	/**
+	 * 
+	    * @Title: findAbsence  
+	    * @Description: 查询请假学生  
+	    * @param @return
+	    * @return List<Check>
+	    * @throws
+	 */
+	public List<Check> findAbsence() {
+		List<Check> list1 = new ArrayList<Check>();
 		List<Check> list;
-		Object[] object =new Object[1];
-		object[0]="0";
+		Object[] object = new Object[1];
+		object[0] = "0";
 		try {
-			list = this.checkDaoImpl.find("from Check where delay =?",object);
-			for(Check r: list) {
-				Check r1=new Check();
+			list = this.checkDaoImpl.find("from Check where delay =?", object);
+			for (Check r : list) {
+				Check r1 = new Check();
 				r1.setId(r.getId());
 				r1.setcTime(r.getcTime());
 				r1.setAbsence(r.getAbsence());
@@ -44,21 +55,27 @@ public class CheckServiceImpl {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			
-		
-	  return list1;
+
+		return list1;
 	}
-	
-	//查询迟到学生
-	public List<Check> findDelay(){
-		List<Check> list1=new ArrayList<Check>();
+
+	/**
+	 * 
+	    * @Title: findDelay  
+	    * @Description:查询迟到学生 
+	    * @param @return
+	    * @return List<Check>
+	    * @throws
+	 */
+	public List<Check> findDelay() {
+		List<Check> list1 = new ArrayList<Check>();
 		List<Check> list;
-		Object[] object =new Object[1];
-		object[0]="0";
+		Object[] object = new Object[1];
+		object[0] = "0";
 		try {
-			list = this.checkDaoImpl.find("from Check where absence =?",object);
-			for(Check r: list) {
-				Check r1=new Check();
+			list = this.checkDaoImpl.find("from Check where absence =?", object);
+			for (Check r : list) {
+				Check r1 = new Check();
 				r1.setId(r.getId());
 				r1.setcTime(r.getcTime());
 				r1.setAbsence(r.getAbsence());
@@ -69,20 +86,34 @@ public class CheckServiceImpl {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			
-		
-	  return list1;
+
+		return list1;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	/**
+	 * 
+	    * @Title: updateCheckAbsence  
+	    * @Description: 插入 请假学生
+	    * @param @param studentDate  请假时间
+	    * @param @param s  请假学生
+	    * @return void
+	    * @throws
+	 */
+	public void updateCheckAbsence(String studentDate, Students s) {
+		this.checkDaoImpl.insertAbsence(studentDate, s);
+	}
+
+	/**
+	 * 
+	    * @Title: updateCheckDelay  
+	    * @Description: 插入迟到学生 
+	    * @param @param studentDate  迟到时间
+	    * @param @param s   迟到学生
+	    * @return void
+	    * @throws
+	 */
+	public void updateCheckDelay(String studentDate, Students s) {
+		this.checkDaoImpl.insertDelay(studentDate, s);
+	}
+
 }
--- weuxin_front_end/wux/pages/student_teacher/student_teacher.js
+++ weuxin_front_end/wux/pages/student_teacher/student_teacher.js
@@ -1,4 +1,3 @@
-// pages/student_teacher/student_teacher.js
 Page({
 
   /**
@@ -6,69 +5,31 @@ Page({
    */
   data: {
     studentDate: "",
-    studentName:" ",
+    studentName: " ",
+    studentDate1: "",
+    studentName1: " ",
+    inputValue: null,
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    var that = this;
-    wx.request({
-      url: 'http://localhost:8080/weixin/delay', //仅为示例，并非真实的接口地址
-
-      header: {
-        'content-type': 'application/x-www-form-urlencoded' // 默认值
-      },
-      method: 'post',
-      success(res) {
-        var fii1 = res.data;
-        console.log(fii1);
-        that.setData({
-          fin1: res.data
-        });
-      },
-      fail: function (res) {
-        console.log(res);
-      }
-    }),
-
-
-      wx.request({
-        url: 'http://localhost:8080/weixin/absence', //仅为示例，并非真实的接口地址
-
-        header: {
-          'content-type': 'application/x-www-form-urlencoded' // 默认值
-        },
-        method: 'post',
-        success(res) {
-          var fii = res.data;
-          console.log(fii);
-          that.setData({
-            fin: res.data
-          });
-        },
-        fail: function (res) {
-          console.log(res);
-        }
-      })
-
-
-
+  onShow: function() {
 
 
   },
@@ -76,47 +37,47 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
 
   },
-  ondutyteacher: function () {
+  ondutyteacher: function() {
     wx.navigateTo({
       url: '../onduty_teacher/onduty_teacher',
     })
   },
-  
 
 
-  courseteacher: function () {
+
+  courseteacher: function() {
     wx.navigateTo({
       url: '../course_teacher/course_teacher',
     })
@@ -124,21 +85,27 @@ Page({
 
 
 
-add: function() {
+  add: function() {
     var that = this;
+    this.setData({
+
+      inputValue: ""
+
+    })
     wx.request({
-      // url: 'http://localhost:8080/weixin/login',
+      url: 'http://localhost:8080/weixin/delayteacher',
       data: {
         studentName: that.data.studentName,
-        studentDate:that.data.studentDate,
+        studentDate: that.data.studentDate,
       },
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
       method: 'post',
       success(res) {
-        var studentName=that.data.studentName;
-        var studentDate=that.data.studentDate;
+        var studentName = that.data.studentName;
+        var studentDate = that.data.studentDate;
+
 
         console.log(res.data);
       },
@@ -149,17 +116,63 @@ add: function() {
 
 
 
+  add1: function() {
+    var that = this;
+    this.setData({
 
-  studentName: function (e) {
+      inputValue: ""
+
+    })
+    wx.request({
+      url: 'http://localhost:8080/weixin/absenceteacher',
+      data: {
+        studentName1: that.data.studentName1,
+        studentDate1: that.data.studentDate1,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      method: 'post',
+      success(res) {
+        var studentName1 = that.data.studentName1;
+        var studentDate1 = that.data.studentDate1;
+
+
+        console.log(res.data);
+      },
+
+    })
+  },
+
+
+
+
+  studentName: function(e) {
     var that = this;
     that.setData({
       studentName: e.detail.value,
     });
+    console.log(this.data.studentName)
   },
-  studentDate: function (e) {
+  studentDate: function(e) {
     var that = this;
     that.setData({
       studentDate: e.detail.value,
+    });
+  },
+
+
+
+  studentName1: function(e) {
+    var that = this;
+    that.setData({
+      studentName1: e.detail.value,
+    });
+  },
+  studentDate1: function(e) {
+    var that = this;
+    that.setData({
+      studentDate1: e.detail.value,
     });
   },
 
