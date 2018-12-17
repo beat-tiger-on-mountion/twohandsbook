735c143ce2c3ec6a0d240c28dcf73e72f096e064
240436170 <240436170@qq.com>
2018-12-17 14:42:36 +0800

    shijian

--- java_backstage/weixin/src/com/weixin/schooltime/controller/SchoolTimeServletImpl.java
+++ java_backstage/weixin/src/com/weixin/schooltime/controller/SchoolTimeServletImpl.java
@@ -50,7 +50,7 @@ public class SchoolTimeServletImpl {
          
     	 st.setGo(upschool);
     	 st.setBack(downschool);
-    	 System.out.println(upschool);
+    	 System.out.println();
     	 schoolTimeServiceImpl.save(st);
 
      }
