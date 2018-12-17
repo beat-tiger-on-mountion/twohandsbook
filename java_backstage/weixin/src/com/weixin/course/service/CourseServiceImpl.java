2a0e2dbd61848d0b178ac5b73053c3aab73c27df
xueyunqing <2404226102@qq.com>
2018-12-17 08:54:01 +0800

    课程表更新

---
+++ java_backstage/weixin/src/com/weixin/course/service/CourseServiceImpl.java
@@ -0,0 +1,24 @@
+//package com.weixin.course.service;
+//
+//import java.util.List;
+//
+//import javax.annotation.Resource;
+//
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import com.weixin.check.dao.CheckDaoImpl;
+//import com.weixin.course.dao.CourseDaoImpl;
+//
+//@Service
+//@Transactional(readOnly = true)
+//public class CourseServiceImpl {
+//	@Resource
+//	private CourseDaoImpl courseDaoImpl;
+//	
+//	
+//	
+//	public void updateCourse(List list0,List list1,List list2,List list3,List list4) {
+//		this.courseDaoImpl.insertCourse(list0, list1, list2, list3, list4);
+//	}
+//}
